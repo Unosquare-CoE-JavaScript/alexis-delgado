cf7848e0c61c58b6592aa8e29f911ca184c43b1d
Alexis Delgado <alexis.delgado@unosquare.com>
2021-03-23 14:04:25 -0700

    External Libraries - Apollo Client

--- react_state_management/src/index.js
+++ react_state_management/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer as App } from './12-redux/App'
+import { AppContainer as App } from './13-apollo/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
