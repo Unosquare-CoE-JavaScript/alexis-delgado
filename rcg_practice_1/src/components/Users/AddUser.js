7bea6e8455693c5f01b7ed66800d2f3727ae6d85
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-13 09:10:31 -0700

    Adding components file, UI, and User. Also AddUser component and Card component

--- rcg_practice_1/src/components/Users/AddUser.js
+++ rcg_practice_1/src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
 import React from "react";
 
+import { Card } from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 export const AddUser = () => {
   const addUserHandler = (event) => event.preventDefault();
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label>Username</label>
-      <input id="username" type="text" />
-      <label>Age(Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label>Username</label>
+        <input id="username" type="text" />
+        <label>Age(Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
