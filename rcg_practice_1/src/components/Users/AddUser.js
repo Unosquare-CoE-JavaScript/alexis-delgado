7f109b3347706b40fc8f4f8d1a3fc1bc1c4cf1f2
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-13 09:43:22 -0700

    Adding state management to username and age

--- rcg_practice_1/src/components/Users/AddUser.js
+++ rcg_practice_1/src/components/Users/AddUser.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Card } from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 export const AddUser = () => {
-  const addUserHandler = (event) => event.preventDefault();
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnterAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnterAge(event.target.value);
+  };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label>Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label>Age(Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
