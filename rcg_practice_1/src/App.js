083b8ace85d57860c9150bdc8a97b9dc790e4f04
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-13 16:51:40 -0700

    Portals

--- rcg_practice_1/src/App.js
+++ rcg_practice_1/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
 import { AddUser } from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
@@ -16,9 +16,9 @@ export const App = () => {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 };
