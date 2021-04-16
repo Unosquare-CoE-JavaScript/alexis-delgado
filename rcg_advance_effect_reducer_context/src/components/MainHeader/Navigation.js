f36ec89d13e1c3893cf71ba4a353586142fdac50
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-16 15:24:52 -0700

    Consuming context with useContext hook

--- rcg_advance_effect_reducer_context/src/components/MainHeader/Navigation.js
+++ rcg_advance_effect_reducer_context/src/components/MainHeader/Navigation.js
@@ -1,36 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth_context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  // consuming context with useContext hook
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        console.log(ctx);
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    // Using context with a consumer
+    // <AuthContext.Consumer>
+    //   {(ctx) => {
+    // return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+    // );
+    //   }}
+    // </AuthContext.Consumer>
   );
 };
 
