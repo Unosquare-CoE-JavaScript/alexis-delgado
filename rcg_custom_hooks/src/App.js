a7dca8893b1864e3fc613169e47e8fcfc7de1153
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-22 15:59:44 -0700

    using useHttp custom hook

--- rcg_custom_hooks/src/App.js
+++ rcg_custom_hooks/src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useState, useEffect, useCallback } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+import useHttp from "./hooks/useHttp";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const key in tasksObj) {
+        loadedTasks.push({
+          id: key,
+          text: tasksObj[key].text,
+        });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-e2060-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, []);
+
   return (
     <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
     </React.Fragment>
   );
 }
