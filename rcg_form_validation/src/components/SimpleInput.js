e316f1c44c6066a495ea273d83e41ea7989c9ede
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-23 10:40:09 -0700

    Getting input values from submission

---
+++ rcg_form_validation/src/components/SimpleInput.js
@@ -0,0 +1,36 @@
+import React, { useState, useRef } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enterName, setEnteredName] = useState("");
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enterName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+  };
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputHandler}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
