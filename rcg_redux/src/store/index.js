6f205d8a681c5a1669348f34fe6c332c8911be82
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-27 10:02:55 -0700

    Adding payload

--- rcg_redux/src/store/index.js
+++ rcg_redux/src/store/index.js
@@ -11,6 +11,12 @@ const counterReducer = (state = initialState, action) => {
     };
   }
 
+  if (action.type === "INCREASE") {
+    return {
+      counter: state.counter + action.amount,
+    };
+  }
+
   if (action.type === "DECREMENT") {
     return { counter: state.counter - 1 };
   }
