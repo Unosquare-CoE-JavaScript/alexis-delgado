dcdd78c423d955d103529a1d9735426d675af587
Alexis Delgado <alexis.delgado@unosquare.com>
2021-04-28 11:40:06 -0700

    Splitting state slices

--- rcg_redux/src/store/index.js
+++ rcg_redux/src/store/index.js
@@ -1,46 +1,8 @@
-import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
-  counter: 0,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import authReducer from "./auth";
+import counterReducer from "./counter";
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === "INCREMENT") {
@@ -74,10 +36,7 @@ const authSlice = createSlice({
 // const store = createStore(counterSlice.reducer);
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
