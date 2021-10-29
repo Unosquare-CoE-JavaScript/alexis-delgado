fc80b5345a74afb81dd808e6bb7d2635b2620b3d
Alexis Delgado <alexis.delgado@unosquare.com>
2021-10-28 17:00:29 -0700

    react fundamentals

---
+++ epicreact/react-fundamentals/src/exercise/05.js
@@ -0,0 +1,68 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+// 💰 Use the className for the size and style (backgroundColor) for the color
+// 💰 each of the elements should also have the "box" className applied
+
+// 🐨 add a className prop to each of these and apply the correct class names
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+
+// 🐨 add a style prop to each of them as well so their background color
+// matches what the text says it should be as well as `fontStyle: 'italic'`
+const smallBox = (
+  <div
+    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+    className={`box box--small`}
+  >
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div
+    style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+    className={'box box--medium'}
+  >
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div
+    style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+    className={'box box--large'}
+  >
+    large orange box
+  </div>
+)
+
+const Box = ({children, size, ...rest}) => (
+  <div className={`box box--${size}`} {...rest}>
+    {children}
+  </div>
+)
+
+function App() {
+  return (
+    <div>
+      <Box
+        style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+        size="small"
+      >
+        small lightblue box
+      </Box>
+      <Box style={{backgroundColor: 'pink', fontStyle: 'italic'}} size="medium">
+        medium pink box
+      </Box>
+      <Box
+        style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+        size="large"
+      >
+        large orange box
+      </Box>
+    </div>
+  )
+}
+
+export default App
